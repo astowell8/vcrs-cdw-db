c9f06ef9f68d142d8bf2cd6b8aff4afaf0e19d0b
AStowell <Andrew.Stowell@Allscripts.com>
2025-08-06 14:46:34 -0400

    Items for Release v1.0.3

--- Database/Tables/dbo.Account_Info.Table.sql
+++ Database/Tables/dbo.Account_Info.Table.sql
@@ -5,10 +5,11 @@ DEVELOPER: Andy S
 HISTORY:
 
   v1.0.2 - Created Table.
+  v1.0.3 - Fix Bug. Foreign Key, If not exists...
 
 ****/
 
-IF NOT EXISTS ( SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Account' )
+IF NOT EXISTS ( SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Account_Info' )
 BEGIN
     CREATE TABLE dbo.Account_Info
     (
@@ -16,7 +17,7 @@ BEGIN
         ,AccountType     VARCHAR(40)  NOT NULL  --User, Elevated, Admin
         ,AccountOpenDTTM DATETIME2(0) NOT NULL
         ,RecordedDTTM    DATETIME2(0) NOT NULL		
-        ,FOREIGN KEY ON (FK_Id) 
+        ,FOREIGN KEY (FK_Id) 
          REFERENCES dbo.Account (Id)
     )
 END
--- Database/Tables/dbo.TrasnsactionHistory.Table.sql
+++ Database/Tables/dbo.TrasnsactionHistory.Table.sql
@@ -6,6 +6,7 @@ HISTORY:
 
   v1.0.0 - Created Table.
   v1.0.1 - New column RecordedDTTM.
+  v1.0.3 - Fix Bug. Foreign Key.
 ****/
 
 IF NOT EXISTS ( SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TransactionHistory' )
@@ -18,7 +19,7 @@ CREATE TABLE dbo.TransactionHistory
 	,TransactionDTTM DATETIME2(0) NOT NULL
 	,RecordedDTTM    DATETIME2(0) NOT NULL
 	,FOREIGN KEY ( AccountId) 
-	 REFERENCES dbo.Account
+	 REFERENCES dbo.Account (ID)
 );
 END
 GO
