a642a1195f7fca6b1b589e9ea961e260a587d7b0
astowell8 <astowell8@hotmail.com>
2025-08-18 18:23:01 -0400

    Added Changes to ps1

---
+++ CDWDiff.ps1
@@ -0,0 +1,28 @@
+
+
+
+#region  ~~  INPUT  ~~
+
+$start_tag = ''
+$end_tag = ''
+
+#Git Repo
+
+$path_cdw = '' # Path to git vcrs-cdw-db folder.
+$path_dbops = '' 
+
+
+#endregion
+
+#region  ~~  DIFF  ~~
+
+Clear-Host
+
+# 1. Verify Folders exist
+# 2. Verify Tags Exist
+
+git checkout main
+git fetch --tags
+git pull
+
+#endregion
